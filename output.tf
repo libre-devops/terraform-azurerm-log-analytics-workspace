b0f234d0bedb67de50124ea22d7f752c01a8e537
Craig Thacker <craigthackerx@gmail.com>
2022-05-11 22:42:05 +0100

    Update module

--- output.tf
+++ output.tf
@@ -8,12 +8,6 @@ output "law_name" {
   description = "The name of the log analytics workspace"
 }
 
-output "law_portal_url" {
-  description = "The portla urlof the log analytics workspace. If a new log analytic workspace is created, fetch its data id, if one is created, fetch the remote one instead"
-
-  value = var.create_new_workspace == true ? data.azurerm_log_analytics_workspace.read_created_law.0.portal_url : data.azurerm_log_analytics_workspace.read_law.0.portal_url
-}
-
 output "law_primary_key" {
   description = "The primary key of the log analytics workspace. If a new log analytic workspace is created, fetch its data id, if one is created, fetch the remote one instead"
   value       = var.create_new_workspace == true ? data.azurerm_log_analytics_workspace.read_created_law.0.primary_shared_key : data.azurerm_log_analytics_workspace.read_law.0.primary_shared_key
