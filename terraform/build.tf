56d3a628a8fb26f560aa89225c162ffd2fd45b2a
Craig Thacker <craigthackerx@gmail.com>
2022-05-17 22:44:29 +0100

    Update

--- terraform/build.tf
+++ terraform/build.tf
@@ -7,3 +7,19 @@ module "rg" {
 
   #  lock_level = "CanNotDelete" // Do not set this value to skip lock
 }
+
+module "law" {
+  source = "registry.terraform.io/libre-devops/log-analytics-workspace/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  create_new_workspace       = true
+  law_name                   = "law-${var.short}-${var.loc}-${terraform.workspace}-01"
+  law_sku                    = "PerNode"
+  retention_in_days          = "30"
+  daily_quota_gb             = "0.5"
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
+}
